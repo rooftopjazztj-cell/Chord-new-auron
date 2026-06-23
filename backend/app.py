ae77ea9624cfd7400bd288368cfb780707695525
rooftopjazztj-cell <rooftopjazztj@gmail.com>
2026-06-22 19:44:44 -0700

    Actualizar app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.get("/test")
+def test():
+    return {
+        "message": "Servidor funcionando correctamente"
+    }
